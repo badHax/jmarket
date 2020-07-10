e89f23bc0841eaf509e7e8d318a29cd293bbff95
Odain Chevannes <odain_chevannes@sagicor.com>
2020-07-10 16:19:19 -0500

    dashboard and Login screen updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { GoogleChartsModule } from 'angular-google-charts'
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { JwtInterceptor} from './_services';
+import { ErrorInterceptor} from './_services';
 
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatCardModule} from "@angular/material/card"
-import {MatIconModule} from "@angular/material/icon"
-
-
-import {JwtInterceptor} from './_services';
-import {ErrorInterceptor} from './_services';
+import { NgApexchartsModule } from 'ng-apexcharts'
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
@@ -23,19 +19,25 @@ import { MainDashComponent } from './main-dash/main-dash.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MainChartComponent } from './main-chart/main-chart.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import { MatProgressSpinnerModule} from '@angular/material/progress-spinner'
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     MainDashComponent,
+    MainChartComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    GoogleChartsModule,
     ToastrModule.forRoot(),
+    NgApexchartsModule,
     MatToolbarModule,
     MatCardModule,
     MatIconModule,
@@ -45,7 +47,8 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatListModule,
     MatGridListModule,
     MatMenuModule,
-    MatTabsModule
+    MatTabsModule,
+    MatProgressSpinnerModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass:JwtInterceptor, multi:true},
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import {AuthenticationService} from '../_services/authentication.service'
 import { ToastrService } from 'ngx-toastr';
 import { Router, ActivatedRoute } from '@angular/router';
 import { first } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -11,11 +12,14 @@ import { first } from 'rxjs/operators';
 })
 export class LoginComponent implements OnInit {
   returnUrl: string;
+  public loading:boolean;
+
   constructor(
     private _auth:AuthenticationService,
     private _toast:ToastrService,
     private _router:Router,
     private _route: ActivatedRoute) {
+      this.loading = false;
       // redirect to home if already logged in
       if (this._auth.currentUserValue) {
         this._router.navigateByUrl('dashboard');
@@ -24,9 +28,12 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
-  loginUser(e:Event){
-    //submit for that has 2 fields username,password
+  toggleLoader(): void{
+    this.loading = !this.loading;
+  }
+  loginUser(e: Event): void{
+    this.toggleLoader();
+    // submit for that has 2 fields username,password
     e.preventDefault();
     console.log(e);
     const username = e.target[0].value;
@@ -37,9 +44,11 @@ export class LoginComponent implements OnInit {
             .subscribe(
                 data => {
                     this._router.navigateByUrl(this.returnUrl);
+                    this.toggleLoader();
                 },
                 error => {
                     this._toast.error(error);
+                    this.toggleLoader();
                 });
 
   }
